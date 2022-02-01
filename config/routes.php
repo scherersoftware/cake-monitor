ac77c0930d7e98ef2fbc44fff42c5f226ad8351d
Jonathan Neugber <jonathan-neugber@users.noreply.github.com>
2022-02-01 10:52:16 +0100

    Fix deprecation warnings for cake 4.3

--- config/routes.php
+++ config/routes.php
@@ -3,7 +3,10 @@
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
-Router::plugin('Monitor', static function (RouteBuilder $builder): void {
-    $builder->connect('/', ['plugin' => 'Monitor', 'controller' => 'Check', 'action' => 'check']);
-    $builder->fallbacks('DashedRoute');
-});
+return static function (RouteBuilder $routes): void
+{
+    $routes->plugin('Monitor', static function (RouteBuilder $builder): void {
+        $builder->connect('/', ['plugin' => 'Monitor', 'controller' => 'Check', 'action' => 'check']);
+        $builder->fallbacks('DashedRoute');
+    });
+};
